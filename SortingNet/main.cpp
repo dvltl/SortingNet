1defafdb9f9248d656bed250a0ea0cb8d45f7056
Anton Volkov <avolkov@ispras.ru>
2016-10-29 16:18:19 +0300

    Initial Commit

---
+++ SortingNet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SortingNet
+//
+//  Created by anton on 29/10/2016.
+//  Copyright © 2016 RandomStuff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
